9612c1ccfc440fb6672388342a538b9bf6223bf1
Mohammad Shahansha <mohammadshahansha@Mohammads-MacBook-Pro.local>
2024-05-02 16:58:13 +0530

    add

---
+++ webpack.config.cjs
@@ -0,0 +1,36 @@
+// import path from "path";
+// import webpackNodeExternals from "webpack-node-externals";
+// import Dotenv from "dotenv-webpack";
+const path = require("path");
+const webpackNodeExternals = require("webpack-node-externals");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./server.js",
+  target: "node",
+  mode: "development",
+  externals: [webpackNodeExternals()],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+      path: "./.env",
+    }),
+  ],
+};
